9621b19416bb4718afa80b6e0cf8c4ee608534a7
Gemsii <gemovictatjana@gmail.com>
2024-05-23 19:12:56 +0200

    Add navigation bar

--- nft-marketplace-frontend/src/App.tsx
+++ nft-marketplace-frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navigation-bar';
+import { Stack } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <Stack flexGrow={1}>
+
+      </Stack>
     </div>
   );
 }
